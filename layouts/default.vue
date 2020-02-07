3772eb22a162aadba7aa9e0060786cee00ff2b24
gaiz <594277665@qq.com>
2020-02-07 15:20:08 +0800

    完成项目的搭建

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header />
+    <nuxt />
+    <Footer/>
+  </div>
+</template>
+<script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+export default {
+  components:{
+    Footer,Header
+  }
+}
+</script>
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+
+</style>
